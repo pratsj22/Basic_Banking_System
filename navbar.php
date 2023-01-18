67ef394977472e662a32cfa6da6aea3a6710a4f8
pratsj22 <prathmeshjain42@gmail.com>
2023-01-18 23:27:19 +0530

    Initial Commit

---
+++ navbar.php
@@ -0,0 +1,33 @@
+<!-- navbar --> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create User</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/table.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+</head>
+<body>
+  <nav class="navbar navbar-expand-md navbar-light" style="background-color: darkblue;border-bottom: 15px solid orange;">
+    <a class="navbar-brand" href="index.php" style="color : orange;font-size:27px;font-weight:bold; font-family:'sans-serif'"><i class="fa fa-bank" style="font-size:36px;color:aliceblue;padding-right: 10px"></i>PMJ BANK</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php" style="color : orange;"><b>Home</b></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="transfermoney.php" style="color : orange;"><b>Transfer Money</b></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="transactionhistory.php" style="color : orange;"><b>Transaction History</b></a>
+        </li>
+      </div>
+    </nav>
+    
+  </body>
